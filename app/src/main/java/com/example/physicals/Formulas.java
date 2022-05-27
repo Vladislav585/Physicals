1e21681c669ef6e0de29e0da7581a827d0648724
Vladislav585 <baranovskiyvladislav515@gmail.com>
2022-05-27 11:47:27 +0300

    Com

--- app/src/main/java/com/example/physicals/Formulas.java
+++ app/src/main/java/com/example/physicals/Formulas.java
@@ -13,7 +13,6 @@ import android.widget.Toast;
 import java.util.Objects;
 
 public class Formulas extends AppCompatActivity {
-    String[] formulas = {"P = mg \nm — масса тела, g — ускорение свободного падения, P - вес тела \nСИ: P = Н, m = кг, g = 9,8 м/с2 \nТакже можно найти: m = P / g, g = P / m"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
